0b631dde705c7abc6a158412a679646b2028b087
akash-divergentsoftlab <akash.poddar@divergentsl.com>
2021-07-12 00:46:39 +0530

    added routing and added categories section according to the category(only UI)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Route } from 'react-router-dom';
+import ShopPage from './pages/shoppage/shop.component';
 
+
+const HatsPage = () => {
+  <div>
+    <h1>This is the Hatspage</h1>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/pages/shoppage/shop.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ShopData from './shop.data';
+import CollectionPreview from './../../components/collection-preview/collection-preview.component';
+
+
+
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: ShopData
+    }
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherCollectionprops }) => (
+            <CollectionPreview key={id}{...otherCollectionprops} />
+          ))
+        },
+      </div>);
+  }
+}
+
+export default ShopPage;
